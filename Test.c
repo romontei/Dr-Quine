60f0c6f2242b6ed0cb4b20d9d2d70af5f74027c9
romontei <romontei@student.42.fr>
2019-02-18 19:58:29 +0100

    Proper version need to merge

--- Test.c
+++ Test.c
@@ -2,11 +2,11 @@
 
 char	*sourceCode()
 {
-	return "#include<stdio.h>%2$c%2$cchar%3$c*sourceCode()%2$c{%2$c%3$creturn %4$c%1$s%4$c;%2$c}%2$c%2$cint%3$cmain()%2$c{%2$c%3$cchar *code = sourceCode();%2$c%3$cprintf(code,10,9,34);%2$c}%2$c";
+	return "#include<stdio.h>%1$c%1$cchar%2$c*sourceCode()%1$c{%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint%2$cmain()%1$c{%1$c%2$cchar *code = sourceCode();%1$c%2$cprintf(code,10,9,34);%1$c}%1$c";
 }
 
 int	main()
 {
 	char *code = sourceCode();
-	printf(code,10,9,34);
+	printf(code,10,9,34,code);
 }
