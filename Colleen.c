ff1056970728758ded48e90055b1bac7f1a07e88
romontei <romontei@student.42.fr>
2019-02-28 09:09:19 +0100

    Handle argumnts of printfs, made code shorter.

--- Colleen.c
+++ Colleen.c
@@ -2,11 +2,11 @@
 
 char	*sourceCode()
 {
-	return "#include<stdio.h>%c%cchar%c*sourceCode()%c{%c%creturn %c%s%c;%c}%c%cint%cmain()%c{%c%cchar *code = sourceCode();%c%cprintf(code,10,10,9,10,10,9,34,code,34,10,10,10,9,10,10,9,10,9,10,10);%c}%c";
+	return "#include<stdio.h>%1$c%1$cchar%2$c*sourceCode()%1$c{%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint%2$cmain()%1$c{%1$c%2$cchar *code = sourceCode();%1$c%2$cprintf(code,10,9,34);%1$c}%1$c";
 }
 
 int	main()
 {
 	char *code = sourceCode();
-	printf(code,10,10,9,10,10,9,34,code,34,10,10,10,9,10,10,9,10,9,10,10);
+	printf(code,10,9,34,code);
 }
