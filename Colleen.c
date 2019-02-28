2d1fb77a0e64f216ec0254c2e79d5ceebb4a9650
romontei <romontei@student.42.fr>
2019-02-28 10:41:36 +0100

    Finish Colleen properly

--- Colleen.c
+++ Colleen.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 
+/*
+  Function that returns the source code
+*/
 char	*sourceCode()
 {
-	return "#include<stdio.h>%1$c%1$cchar%2$c*sourceCode()%1$c{%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint%2$cmain()%1$c{%1$c%2$cchar *code = sourceCode();%1$c%2$cprintf(code,10,9,34);%1$c}%1$c";
+	return "#include<stdio.h>%1$c%1$c/*%1$c  Function that returns the source code%1$c*/%1$cchar%2$c*sourceCode()%1$c{%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint%2$cmain()%1$c{%1$c%2$c/*%1$c%2$c  Stores source code then print it%1$c%2$c*/%1$c%2$cchar *code = sourceCode();%1$c%2$cprintf(code,10,9,34,code);%1$c}%1$c";
 }
 
 int	main()
 {
+	/*
+	  Stores source code then print it
+	*/
 	char *code = sourceCode();
 	printf(code,10,9,34,code);
 }
