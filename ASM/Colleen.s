714fd27d5ccda03a179700b6c91aca535102ae3f
romontei <romontei@student.42.fr>
2019-03-01 13:50:03 +0100

    Start ASM part with Colleen.

---
+++ ASM/Colleen.s
@@ -0,0 +1,24 @@
+[BITS 64]
+
+section .data
+hello:
+	.string db "Le petit Colleen!", 10
+	.len equ $ - hello.string
+
+section .text
+	global start
+	global _main
+	extern _printf
+
+start:
+	call _main
+	ret
+
+_main:
+	push rbp
+	mov rbp, rsp
+	sub rsp, 16
+	lea rdi, [rel hello.string]
+	call _printf
+	leave
+	ret
