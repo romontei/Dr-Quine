69b5abc250bf9c71733acf654e0ef00441e24648
romontei <romontei@student.42.fr>
2019-03-15 15:21:37 +0100

    Stack align in Colleen and fix Sully

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,7 +1,7 @@
 ;Pretty much the same as C version so meh
 section .data
 hello:
-	.string db "%4$cPretty much the same as C version so meh%1$csection .data%1$chello:%1$c%2$c.string db %3$c%5$s%3$c%1$c%1$csection .text%1$c%2$cglobal start%1$c%2$cglobal _main%1$c%2$cextern _printf%1$c%1$cstart:%1$c%2$ccall _main%1$c%2$cret%1$c%1$c_main:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%2$c%4$cStores printf args%1$c%2$clea rdi, [rel hello.string]%1$c%2$cmov rsi, 10%1$c%2$cmov rdx, 9%1$c%2$cmov rcx, 34%1$c%2$cmov r8, 59%1$c%2$clea r9, [rel hello.string]%1$c%2$ccall _printf%1$c%2$cleave%1$c%2$cret%1$c"
+	.string db "%4$cPretty much the same as C version so meh%1$csection .data%1$chello:%1$c%2$c.string db %3$c%5$s%3$c%1$c%1$csection .text%1$c%2$cglobal start%1$c%2$cglobal _main%1$c%2$cextern _printf%1$c%1$cstart:%1$c%2$ccall _main%1$c%2$cret%1$c%1$c_main:%1$c%2$cpush rbp%1$c%2$cmov rbp, rsp%1$c%2$csub rsp, 16%1$c%2$c%4$cStores printf args%1$c%2$clea rdi, [rel hello.string]%1$c%2$cmov rsi, 10%1$c%2$cmov rdx, 9%1$c%2$cmov rcx, 34%1$c%2$cmov r8, 59%1$c%2$clea r9, [rel hello.string]%1$c%2$ccall _printf%1$c%2$cleave%1$c%2$cret%1$c"
 
 section .text
 	global start
@@ -15,6 +15,7 @@ start:
 _main:
 	push rbp
 	mov rbp, rsp
+	sub rsp, 16
 	;Stores printf args
 	lea rdi, [rel hello.string]
 	mov rsi, 10
--- ASM/Sully.s
+++ ASM/Sully.s
@@ -6,7 +6,7 @@
 
 %macro OPEN 0
     lea rdi, [rel file.name]
-    mov rsi, 0x0202
+    mov rsi, 0x601
     mov rdx, 644o
     mov rax, 0x2000005
     syscall
@@ -27,6 +27,8 @@
 %macro BUILD 0
     lea rdi, [rel file.build]
     call _system
+    lea rdi, [rel file.del]
+    call _system
 %endmacro
 
 %macro EXEC 0
@@ -37,12 +39,13 @@
 %macro MAIN 0
     section .data
     code:
-        .string db "%%define i %5$d%1$c%%assign CPT i-1%1$c%%defstr NASM /Users/romontei/.brew/bin/nasm -f macho64%1$c%%defstr ARCH -macosx_version_min 10.11 -lSystem%1$c%%defstr NAME Sully_ %%+i%1$c%1$c%%macro OPEN 0%1$c    lea rdi, [rel file.name]%1$c    mov rsi, 0x0202%1$c    mov rdx, 644o%1$c    mov rax, 0x2000005%1$c    syscall%1$c%%endmacro%1$c%1$c%%macro PRINT 2%1$c    lea rsi, [rel %%1]%1$c    mov rdx, 10%1$c    mov rcx, 59%1$c    mov r8, 34%1$c    lea r9, [rel %%1]%1$c    mov r10, %%2%1$c    push r10%1$c    call _dprintf%1$c    pop r10%1$c%%endmacro%1$c%1$c%%macro BUILD 0%1$c    lea rdi, [rel file.build]%1$c    call _system%1$c%%endmacro%1$c%1$c%%macro EXEC 0%1$c    lea rdi, [rel file.exec]%1$c    call _system%1$c%%endmacro%1$c%1$c%%macro MAIN 0%1$c    section .data%1$c    code:%1$c        .string db %3$c%4$s%3$c, 0%1$c%1$c    file:%1$c        .name db NAME, %3$c.s%3$c, 0%1$c        .build db NASM, 32, NAME, %3$c.s%3$c, %3$c -o %3$c, NAME, %3$c.o%3$c, %3$c && %3$c, %3$cld %3$c, NAME, %3$c.o %3$c, ARCH, %3$c -o %3$c, NAME, 0%1$c        .exec db %3$c./%3$c, NAME, 0%1$c%1$c    section .text%1$c        global start%1$c        global _main%1$c        extern _dprintf%1$c        extern _system%1$c%1$c    start:%1$c        call _main%1$c        ret%1$c%1$c    _main:%1$c        push rbp%1$c        mov rbp, rsp%1$c        sub rsp, 16%1$c        OPEN%1$c        mov rdi, rax%1$c        push rax%1$c        PRINT code.string, CPT%1$c        mov rax, 0x2000006%1$c        pop rdi%1$c        syscall%1$c        BUILD%1$c        mov rax, i%1$c        cmp rax, 0%1$c        jle end%1$c        EXEC%1$c        OPEN%1$c        mov rdi, rax%1$c        push rax%1$c        PRINT code.string, i%1$c        mov rax, 0x2000006%1$c        call end%1$c    end:%1$c        leave%1$c        ret%1$c%%endmacro%1$c%1$c%2$c Call the define that lets the magic begin%1$cMAIN%1$c", 0
+        .content db "%%define i %5$d%1$c%%assign CPT i-1%1$c%%defstr NASM /Users/romontei/.brew/bin/nasm -f macho64%1$c%%defstr ARCH -macosx_version_min 10.11 -lSystem%1$c%%defstr NAME Sully_ %%+i%1$c%1$c%%macro OPEN 0%1$c    lea rdi, [rel file.name]%1$c    mov rsi, 0x601%1$c    mov rdx, 644o%1$c    mov rax, 0x2000005%1$c    syscall%1$c%%endmacro%1$c%1$c%%macro PRINT 2%1$c    lea rsi, [rel %%1]%1$c    mov rdx, 10%1$c    mov rcx, 59%1$c    mov r8, 34%1$c    lea r9, [rel %%1]%1$c    mov r10, %%2%1$c    push r10%1$c    call _dprintf%1$c    pop r10%1$c%%endmacro%1$c%1$c%%macro BUILD 0%1$c    lea rdi, [rel file.build]%1$c    call _system%1$c    lea rdi, [rel file.del]%1$c    call _system%1$c%%endmacro%1$c%1$c%%macro EXEC 0%1$c    lea rdi, [rel file.exec]%1$c    call _system%1$c%%endmacro%1$c%1$c%%macro MAIN 0%1$c    section .data%1$c    code:%1$c        .content db %3$c%4$s%3$c, 0%1$c%1$c    file:%1$c        .name db NAME, %3$c.s%3$c, 0%1$c        .build db NASM, 32, NAME, %3$c.s%3$c, %3$c -o %3$c, NAME, %3$c.o%3$c, %3$c && %3$c, %3$cld %3$c, NAME, %3$c.o %3$c, ARCH, %3$c -o %3$c, NAME, 0%1$c        .exec db %3$c./%3$c, NAME, 0%1$c        .del db %3$crm %3$c, NAME, %3$c.o%3$c, 0%1$c%1$c    section .text%1$c        global start%1$c        global _main%1$c        extern _dprintf%1$c        extern _system%1$c%1$c    start:%1$c        call _main%1$c        ret%1$c%1$c    _main:%1$c        push rbp%1$c        mov rbp, rsp%1$c        sub rsp, 16%1$c        OPEN%1$c        mov rdi, rax%1$c        push rax%1$c        PRINT code.content, CPT%1$c        mov rax, 0x2000006%1$c        pop rdi%1$c        syscall%1$c        BUILD%1$c        OPEN%1$c        mov rdi, rax%1$c        push rax%1$c        PRINT code.content, i%1$c        mov rax, 0x2000006%1$c        pop rdi%1$c        syscall%1$c        mov rdx, i%1$c        cmp rdx, 0%1$c        jle end%1$c        EXEC%1$c        call end%1$c    end:%1$c        leave%1$c        ret%1$c%%endmacro%1$c%1$c%2$c Call the define that lets the magic begin%1$cMAIN%1$c", 0
 
     file:
         .name db NAME, ".s", 0
         .build db NASM, 32, NAME, ".s", " -o ", NAME, ".o", " && ", "ld ", NAME, ".o ", ARCH, " -o ", NAME, 0
         .exec db "./", NAME, 0
+        .del db "rm ", NAME, ".o", 0
 
     section .text
         global start
@@ -61,20 +64,22 @@
         OPEN
         mov rdi, rax
         push rax
-        PRINT code.string, CPT
+        PRINT code.content, CPT
         mov rax, 0x2000006
         pop rdi
         syscall
         BUILD
-        mov rax, i
-        cmp rax, 0
-        jle end
-        EXEC
         OPEN
         mov rdi, rax
         push rax
-        PRINT code.string, i
+        PRINT code.content, i
         mov rax, 0x2000006
+        pop rdi
+        syscall
+        mov rdx, i
+        cmp rdx, 0
+        jle end
+        EXEC
         call end
     end:
         leave
